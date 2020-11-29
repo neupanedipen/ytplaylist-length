adeee2bd5ab1bf15c7b203b16f51c45077244fe3
dipendra <deardipen50@gmail.com>
2020-11-29 21:38:54 +0545

    working prototype of playlist duration finder

---
+++ index.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+const path = require('path')
+const express = require('express');
+const PlaylistSummary = require('youtube-playlist-summary');
+const moment = require('moment')
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser')
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, './public')
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(publicDirectoryPath))
+
+
+const config = {
+    GOOGLE_API_KEY: process.env.API_KEY, // require
+    PLAYLIST_ITEM_KEY: ['publishedAt', 'title', 'description', 'videoId', 'videoUrl'], // option
+}
+
+const ps = new PlaylistSummary(config)
+
+let allVideoIds = [];
+let allDurations = [];
+var len;
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/duration', (req, res) => {
+    const playlistID = req.query.playlistId;
+    console.log(playlistID)
+    if (!playlistID) {
+        return res.send({
+            error: "You must provide an address"
+        })
+    }
+    try {
+        const getVideoDuration = (id, len) => {
+            const getData = async () => {
+                const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails%2Cstatistics&id=${id}&key=${process.env.API_KEY}`)
+                const data = await response.json()
+                timeInMS = moment.duration(data.items[0].contentDetails.duration).asMilliseconds();
+
+                allDurations.push(timeInMS)
+                if (allDurations.length === len) {
+                    const totalDuration = allDurations.reduce((acc, curr) => {
+                        return acc + curr;
+                    })
+                    const totalDurationinSec = (totalDuration / 1000);
+                    console.log(totalDurationinSec)
+                    res.json({
+                        totalDurationinSec: totalDurationinSec
+                    })
+                }
+            }
+
+            getData()
+        }
+        // 
+        const getVideoIds = async () => {
+            const result = await ps.getPlaylistItems(playlistID);
+            len = result.items.length
+
+            allVideoIds = result.items.map(item => item.videoId)
+            allVideoIds.map(id => {
+                getVideoDuration(id, len)
+            })
+
+        }
+
+        console.log(getVideoIds());
+    }
+    catch (err) {
+        res.send({
+            error: err
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log("listenting")
+})
---
+++ public/index.js
@@ -0,0 +1,36 @@
+console.log("From Public Folder")
+
+const playlistForm = document.querySelector('form')
+const input = document.querySelector('input')
+const messageOne = document.querySelector('#messageOne');
+const messageTwo = document.querySelector('#messageTwo');
+
+
+playlistForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const playlistId = input.value;
+
+    fetch(`/duration/?playlistId=${playlistId}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                messageOne.textContent = ""
+                messageTwo.textContent = data.error;
+                input.value = '';
+            } else {
+                messageOne.textContent = ""
+                messageTwo.textContent = "";
+                let time = timeFromSec(parseInt(data.totalDurationinSec))
+                messageOne.innerHTML = `Total Duration Of Playlist= ${time}`
+                input.value = '';
+            }
+        })
+
+})
+
+function timeFromSec(sec) {
+    let hours = Math.floor(sec / 3600);
+    let min = Math.floor((sec % 3600) / 60);
+    let seconds = sec % 60;
+    return `${hours} Hours, ${min} Minutes, ${seconds} Seconds`
+}
